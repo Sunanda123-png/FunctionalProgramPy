91fd327d683a87365b45be320c2ddc0fc2367c00
Sunanda123-png <sunanda02@gmail.com>
2021-12-06 16:24:46 +0530

    Add:changes

--- VowelOrConsonent.py
+++ VowelOrConsonent.py
@@ -1,4 +1,8 @@
 def vowel(character):
+    """
+    :param character:
+    :return:
+    """
     if (character == 'A' or character == 'a' or character == 'E' or character == 'e' or character == 'I'
             or character == 'i' or character == 'O' or character == 'o' or character == 'U' or character == 'u'):
         return True
@@ -6,8 +10,12 @@ def vowel(character):
     return False
 
 if __name__ == "__main__":
-    character = input("Enter a character: ")
-    if vowel(character) == True:
-        print("This is vowel")
-    else:
-        print("This is consonent")
+    try:
+        character = input("Enter a character: ")
+        if vowel(character) == True:
+            print("This is vowel")
+        else:
+            print("This is consonent")
+    except:
+        print("Enter valid input")
+
